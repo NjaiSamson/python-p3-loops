107a410dccacd1da63d920c1166ce73c23df20d0
Samson Njai <njaisamson@gmail.com>
2024-08-27 19:45:39 +0300

    Creates the required functions

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while num > 0:
+        print(num)
+        num -=1
+    print("Happy New Year!")
+
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list_squared = [int_num ** 2 for int_num in int_list ]
+    return int_list_squared
+
+square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        num += 1
+
+fizzbuzz()
